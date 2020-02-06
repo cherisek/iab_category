b245c09a65c82c48fe9249064ead7f71be7ebcb2
Cherise Knox <cherise@gumgum.com>
2020-02-05 17:23:30 -0800

    added page header and search area for home page

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,28 +1,23 @@
 import React, { Fragment } from "react";
-// import logo from './images/logo.png'; 
-import logo from '../images/logo.png';
+import logo from '../images/logo-white.svg';
 
 function Layout(props) {
     const children = props.children; 
     return (
         <Fragment>
-          <header className="gds-spa-header">
+          <header className="gds-page-header">
           <div className="gds-layout-container">
-            <img className="logo" src={logo} alt="gumgum logo"/>  
-              <form>
-                <input
-                    type="text"
-                    className="input"
-                    placeholder="Search for category"
-                />
-                <button className="search-button">
-                    Search 
-                </button>
-                <button className="view-all-button">
-                    View All 
-                </button>
-             </form> 
-             </div>      
+            <img className="logo" src={logo} alt="gumgum logo"/> 
+            <div className="gds-form-group"> 
+              <div className="gds-form-group__input-group">
+                <div className="gds-search-select__control -m-r-2" data-gds-search-select="single" style={{'display': 'flex', 'width': '100%'}}>
+                  <input className="gds-search-select__input--sm -m-r-1" type="text" placeholder="Search for Category"/> 
+                  <button className="gds-button gds-button--xs gds-button--primary -m-r-1">Search</button>
+                  <button className="gds-button gds-button--xs gds-button--primary">View All</button>
+                </div>
+              </div>
+            </div>
+          </div>      
           </header>
           <section className="gds-persist-nav__main-content">
           <div className="gds-layout__container gds-layout__container--full-width -p-v-4">
