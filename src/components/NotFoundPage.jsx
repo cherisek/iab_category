db12e67fa1afe59c15ebedd51543f4019e4acaf4
Cherise Knox <cheriseknox@gmail.com>
2020-03-25 16:33:28 -0700

    final changes made to css for not found page

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -5,7 +5,7 @@ class notFoundPage extends Component {
   render() {
     return (
       <div className="-p-a-2 gds-flex gds-flex--align-center" style={{'height': '100%'}}>
-        <div className="gds-flex gds-flex__item -text-center" style={{'maxWidth': '1200px', 'marginLeft': 'auto', 'marginRight': 'auto', 'flexDirection': 'column', 'alignItems': 'center', 'justifyContent': 'center'}}> 
+        <div className="gds-flex__item -text-center" style={{'top': '30%'}}> 
           <i
               className=
               'gds-text--header-lg -color-tx-sec fa fa-fw fa-exclamation-triangle -inline-block'
