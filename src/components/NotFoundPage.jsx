53b8b6fcab53d71d19bec5c600c170d81304cec8
Cherise Knox <cheriseknox@gmail.com>
2020-03-25 16:15:34 -0700

    adjusted css for page not found

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -4,17 +4,15 @@ import { Link } from 'react-router-dom';
 class notFoundPage extends Component {
   render() {
     return (
-      <div className="-p-a-2 gds-flex gds-flex--align-center" style={{'height': '100%'}}>
-        <div className="gds-flex__item -text-center"> 
-          <i
-              className=
-              'gds-text--header-lg -color-tx-sec fa fa-fw fa-exclamation-triangle -inline-block'
-            
-          />
-          <h1 className="gds-text--header-sm">404: Page Not Found</h1>
-          <p>Uh, Oh! The page you are looking for doesn't exist!</p>
-          <Link to="/" className="-color-tx-pri">Go to Home</Link>
-        </div> 
+      <div className="-p-a-2 gds-flex gds-flex--direction-col gds-flex--align-center -text-cente" style={{'height': '100%'}}>
+        <i
+            className=
+            'gds-text--header-lg -color-tx-sec fa fa-fw fa-exclamation-triangle -inline-block'
+          
+        />
+        <h1 className="gds-text--header-sm">404: Page Not Found</h1>
+        <p>Uh, Oh! The page you are looking for doesn't exist!</p>
+        <Link to="/" className="-color-tx-pri">Go to Home</Link>
      </div>
     )
   }
