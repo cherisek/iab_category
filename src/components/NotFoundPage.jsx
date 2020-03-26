ffaafb6e25634ade9d7309b2051e4d7ee4343f18
Cherise Knox <cheriseknox@gmail.com>
2020-03-26 10:36:13 -0700

    updated typo for not found page

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -5,7 +5,7 @@ class notFoundPage extends Component {
   render() {
     return (
       <div className="-p-a-2 gds-flex gds-flex--align-center" style={{'height': '100%'}}>
-        <div className="gds-flex__item -text-center" style={{'top': '30%'}}> 
+        <div className="gds-flex__item -text-center" style={{'top': '30%'}}>xw
           <i
               className=
               'gds-text--header-lg -color-tx-sec fa fa-fw fa-exclamation-triangle -inline-block'
@@ -13,7 +13,7 @@ class notFoundPage extends Component {
           />
           <h1 className="gds-text--header-sm">404: Page Not Found</h1>
           <p>Uh, Oh! The page you are looking for doesn't exist!</p>
-          <Link to="/" className="-color-tx-pri">Go to Home</Link>
+          <Link to="/" className="-color-tx-pri">Go To home</Link>
         </div> 
      </div>
     )
---
+++ src/components/SearchSingleSelectV2.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import PropTypes from 'prop-types';
+
+
+class SearchSingleSelectV2 extends Component {
+  render() {
+    return (
+      <div className="-p-a-2 gds-flex gds-flex--align-center" style={{'height': '100%'}}>
+     </div>
+    )
+  }
+} 
+
+export default SearchSingleSelect; 
