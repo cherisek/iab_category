2cd177bbe9b9d6459e325b53f7a7742eb9a08c87
Cherise Knox <cheriseknox@gmail.com>
2020-03-30 14:05:29 -0700

    fixed not found page type

--- src/components/NotFoundPage.jsx
+++ src/components/NotFoundPage.jsx
@@ -5,11 +5,10 @@ class notFoundPage extends Component {
   render() {
     return (
       <div className="-p-a-2 gds-flex gds-flex--align-center" style={{'height': '100%'}}>
-        <div className="gds-flex__item -text-center" style={{'top': '30%'}}>xw
+        <div className="gds-flex__item -text-center" style={{'top': '30%'}}>
           <i
               className=
-              'gds-text--header-lg -color-tx-sec fa fa-fw fa-exclamation-triangle -inline-block'
-            
+              "gds-text--header-lg -color-tx-sec fa fa-fw fa-exclamation-triangle -inline-block"    
           />
           <h1 className="gds-text--header-sm">404: Page Not Found</h1>
           <p>Uh, Oh! The page you are looking for doesn't exist!</p>
